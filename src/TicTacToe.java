0f3c5726a6930219c31135377d7f1a65f317b8fe
UCscottg8 <134948683+UCscottg8@users.noreply.github.com>
2023-07-23 12:41:30 -0400

    ver 2

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,37 +1,53 @@
-
 import java.util.Scanner;
 
 public class TicTacToe {
 
-    static char[][] board = new char[3][3];
+    private static final int ROW = 3;
+    private static final int COL = 3;
+    private static String[][] board = new String[ROW][COL];
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        // Initialize the board with spaces
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = ' ';
+        clearBoard();
+        String currentPlayer = "X";
+
+        while (true) {
+            display();
+
+            int row;
+            int col;
+            do {
+                row = SafeInput.getRangedInt(scanner, "Enter the row (1-3) for " + currentPlayer + "'s move", 1, 3) - 1;
+                col = SafeInput.getRangedInt(scanner, "Enter the column (1-3) for " + currentPlayer + "'s move", 1, 3) - 1;
+            } while (!isValidMove(row, col));
+
+            board[row][col] = currentPlayer;
+
+            if (isWin(currentPlayer)) {
+                display();
+                System.out.println("Player " + currentPlayer + " wins!");
+                break;
+            } else if (isTie()) {
+                display();
+                System.out.println("It's a tie!");
+                break;
+            }
+
+            currentPlayer = currentPlayer.equals("X") ? "O" : "X";
+        }
+    }
+    private static void clearBoard() {
+        for (int i = 0; i < ROW; i++) {
+            for (int j = 0; j < COL; j++) {
+                board[i][j] = " ";
             }
         }
-
-        char currentPlayer = 'X';
-        boolean gameOver = false;
-
-        while (!gameOver) {
-            printBoard();
-            makeMove(currentPlayer);
-            gameOver = checkWin(currentPlayer);
-            currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
-        }
-
-        printBoard();
-        System.out.println("Game over! " + ((currentPlayer == 'X') ? 'O' : 'X') + " is the winner!");
     }
 
-    public static void printBoard() {
-        for (int i = 0; i < 3; i++) {
+    private static void display() {
+        for (int i = 0; i < ROW; i++) {
             System.out.println("-------------");
-            for (int j = 0; j < 3; j++) {
+            for (int j = 0; j < COL; j++) {
                 System.out.print("| " + board[i][j] + " ");
             }
             System.out.println("|");
@@ -39,36 +55,50 @@ public class TicTacToe {
         System.out.println("-------------");
     }
 
-    public static void makeMove(char player) {
-        int row;
-        int col;
-        do {
-            row = SafeInput.getRangedInt(scanner, "Enter the row (1-3) for " + player + "'s move", 1, 3) - 1;
-            col = SafeInput.getRangedInt(scanner, "Enter the column (1-3) for " + player + "'s move", 1, 3) - 1;
-        } while (board[row][col] != ' ');
-
-        board[row][col] = player;
+    private static boolean isValidMove(int row, int col) {
+        return " ".equals(board[row][col]);
     }
 
-    public static boolean checkWin(char player) {
-        // Check rows, columns, and diagonals for a win
-        for (int i = 0; i < 3; i++) {
-            if (board[i][0] == player && board[i][1] == player && board[i][2] == player) {
+    private static boolean isWin(String player) {
+        return isRowWin(player) || isColWin(player) || isDiagonalWin(player);
+    }
+
+    private static boolean isRowWin(String player) {
+        for (int i = 0; i < ROW; i++) {
+            if (player.equals(board[i][0]) && player.equals(board[i][1]) && player.equals(board[i][2])) {
                 return true;
             }
-            if (board[0][i] == player && board[1][i] == player && board[2][i] == player) {
-                return true;
-            }
-        }
-        if (board[0][0] == player && board[1][1] == player && board[2][2] == player) {
-            return true;
-        }
-        if (board[0][2] == player && board[1][1] == player && board[2][0] == player) {
-            return true;
         }
         return false;
     }
+
+    private static boolean isColWin(String player) {
+        for (int i = 0; i < COL; i++) {
+            if (player.equals(board[0][i]) && player.equals(board[1][i]) && player.equals(board[2][i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isDiagonalWin(String player) {
+        boolean diagonal1 = player.equals(board[0][0]) && player.equals(board[1][1]) && player.equals(board[2][2]);
+        boolean diagonal2 = player.equals(board[0][2]) && player.equals(board[1][1]) && player.equals(board[2][0]);
+        return diagonal1 || diagonal2;
+    }
+
+    private static boolean isTie() {
+        for (int i = 0; i < ROW; i++) {
+            for (int j = 0; j < COL; j++) {
+                if (" ".equals(board[i][j])) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
+
 
 
 
